376d714819c2cff5905be0473ff89e2cc792c8db
fabricioaquino <fabricio_rbfc@hotmail.com>
2025-10-16 22:07:27 -0300

    Nome do autor aceitar somente letras

--- app/Http/Requests/AutorFormRequest.php
+++ app/Http/Requests/AutorFormRequest.php
@@ -22,7 +22,8 @@ class AutorFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nome' => 'required|string|max:40',
+            // aceita apenas letras, espaços e acentuação
+            'nome' => 'required|string|max:40|regex:/^[\p{L}\s]+$/u',
         ];
     }
 
@@ -32,6 +33,7 @@ class AutorFormRequest extends FormRequest
             'nome.required' => 'O nome do autor é obrigatório.',
             'nome.string'   => 'O nome deve ser um texto válido.',
             'nome.max'      => 'O nome não pode ter mais que 40 caracteres.',
+            'nome.regex'    => 'O nome só pode conter letras e espaços.',
         ];
     }
 }
